d0606a3978a6b8ebd6d26958752dff3570e85829
Yakup <yakupjoraev@gmail.com>
2024-05-12 14:29:01 +0300

    added new pages

---
+++ build/js/scripts.js
@@ -0,0 +1,19 @@
+// Custom Scripts
+// Custom scripts
+
+
+function select() {
+  const container = document.querySelector('.select');
+
+  if (!container) {
+    return null
+  }
+
+
+  const btn = container.querySelector('.select__btn');
+  btn.addEventListener('click', () => {
+    container.classList.toggle('open')
+  })
+}
+
+select();
